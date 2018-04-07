6c10c18934a8c92ebedb48d88bb0850c116c8a90
cheetah <inchaiya.keta@gmail.com>
2018-04-07 02:32:52 +0100

    Dealt with 'Lisa may have drunk milk' sentence

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ ugrammar = FeatureGrammar.fromstring("""\
     WH -> 'when'
     
     # Auxiliary
-    AUX[NUM=sing] -> 'does'
-    AUX[NUM=plur] -> 'do'
+    AUX[NUM=sing] -> 'does' | 'has'
+    AUX[NUM=plur] -> 'do'   | 'have'
     
-    # Noun Phrase
+    # Modal
+    MODP[NUM=plur] -> MOD AUX[NUM=plur]
+    MOD -> 'may'
+    
+    # Noun
     NP[NUM=?n]   -> NOM | DET[NUM=?n] NOM | PROP_N[NUM=?n]
     NP[NUM=plur] -> PROP_N[NUM=?n] CONJ NP[NUM=?n]
     # Nominal
     NOM -> NOM PP
-    NOM -> ADJ NOM | N[NUM=?n]
+    NOM -> ADJ NOM | N[NUM=?n] | GER
+    
+    # Gerund
+    GER -> IV[FORM=pastpart] | TV[FORM=prespart] NP
     
     # Adjective
     ADJ -> 'blue' | 'healthy' | 'green'
@@ -29,10 +36,10 @@ ugrammar = FeatureGrammar.fromstring("""\
     # Conjunction
     CONJ -> 'and'
     
-    # Verb Phrase
-    VP[FORM=?t, NUM=?n] -> ADV VP[FORM=?t, NUM=?n]
+    # Verb
+    VP[FORM=?t, NUM=?n] -> ADV VP[FORM=?t, NUM=?n] | MODP[NUM=plur] VP[FORM=?t, NUM=plur]
     VP[FORM=?t, NUM=?n] -> IV[FORM=?t, NUM=?n]
-    VP[FORM=?t, NUM=?n] -> TV[FORM=?t, NUM=?n] NP | TV[FORM=?t, NUM=?n] S | TV[FORM=?t, NUM=?n] NP NP
+    VP[FORM=?t, NUM=?n] -> TV[FORM=?t, NUM=?n] NP | TV[FORM=?t, NUM=?n] NP NP | TV[FORM=?t, NUM=?n] S 
     
     # Adverb
     ADV -> 'always' | 'never'
@@ -47,11 +54,15 @@ ugrammar = FeatureGrammar.fromstring("""\
     N -> 'milk' | 'midnight'
     
     IV[FORM=base, NUM=plur]     -> 'laugh'
-    TV[FORM=base, NUM=plur]     -> 'drink'  | 'wear'    | 'serve'   | 'think' | 'likes'
+    TV[FORM=base, NUM=plur]     -> 'drink'      | 'wear'    | 'serve'   | 'think' | 'like'
     IV[FORM=vbz,  NUM=sing]     -> 'laughs'
-    TV[FORM=vbz,  NUM=sing]     -> 'drinks' | 'wears'   | 'serves'  | 'thinks' | 'like'
+    TV[FORM=vbz,  NUM=sing]     -> 'drinks'     | 'wears'   | 'serves'  | 'thinks' | 'likes'
     IV[FORM=pret]               -> 'laughed'
-    TV[FORM=pret]               -> 'drank'  | 'wore'    | 'served'  | 'thought' | 'liked'
+    TV[FORM=pret]               -> 'drank'      | 'wore'    | 'served'  | 'thought' | 'liked'
+    IV[FORM=pastpart]           -> 'laughed'
+    TV[FORM=pastpart]           -> 'drunk'      | 'worn'    | 'served'  | 'thought' | 'liked'
+    IV[FORM=prespart]           -> 'laughing'
+    TV[FORM=prespart]           -> 'drinking'   | 'wearing'    | 'serving'  | 'thinking' | 'liking'
 """)
 
 uparser = FeatureChartParser(ugrammar)
@@ -70,6 +81,14 @@ when Homer drinks milk Bart laughs
 when does Lisa drink the milk on the table
 when do Lisa and Bart wear shoes
 Bart likes drinking milk
+Lisa may have drunk milk
+Lisa may have seen Bart drinking milk
+Lisa may not have seen Bart drinking milk
+what does Homer drink
+what salad does Bart serve
+whom does Homer serve salad
+whom do Homer and Lisa serve
+what salad does Bart think Homer serves Lisa
 """
 sents = text.splitlines()
 for sent in sents:
