0f1a4332060a584ba305df980a761ee7036033df
cheetah <inchaiya.keta@gmail.com>
2018-04-08 16:07:55 +0100

    Dealt with 'Lisa claims that Bart always leaves before 8' sentence and cleaned the grammar

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from nltk.grammar import FeatureGrammar
 from nltk import FeatureChartParser
 
 ugrammar = FeatureGrammar.fromstring("""\
-    # Sentence
+    ###################################################################################### 
+    ################################### Sentence #########################################
+    ######################################################################################
     S -> NP[NUM=?n] VP[NUM=?n] 
     S -> WH AUX[FORM=?t, NUM=?n] NP[NUM=?n] VP | WH NP[NUM=?n] AUX[FORM=?t, NUM=?n] NP[NUM=?n] VP | WH NP[NUM=?n] VP S
     S ->    AUX[FORM=?t, NUM=?n] NP[NUM=?n] VP
     
-    # WH-questions
+    ######### WH-questions ##########
     WH -> 'what' | 'when' | 'where' | 'why' | 'who' | 'whom'
     
     ######################################################################################
@@ -16,7 +18,7 @@ ugrammar = FeatureGrammar.fromstring("""\
     NP[NUM=?n]   -> PRE_DET NP[NUM=?n]| DET[NUM=?n] NOM | NOM
     NP[NUM=plur] -> PROP_N[NUM=?n] CONJ NP[NUM=?n]
     
-    ######### Predeterminers #########
+    ######### Predeterminers ##########
     PRE_DET       -> 'all' | 'most'
     
     ######### Determiners #############
@@ -37,18 +39,18 @@ ugrammar = FeatureGrammar.fromstring("""\
     ######## Nouns #####################
     N[NUM=sing] -> 'shoe'   | 'kitchen'     | 'table'   | 'salad' | 'plane'  | 'flight' | 'train'  | 'house' | 'person'
     N[NUM=plur] -> 'shoes'  | 'kitchens'    | 'tables'  | 'salad' | 'planes' | 'flights'| 'trains' | 'houses'| 'people'
-    N -> 'milk' | 'morning' | 'midnight' | 'Edinburgh' | 'London' | '9' | '10' | 'breakfast'
+    N -> 'milk' | 'morning' | 'midnight' | 'Edinburgh' | 'London' | '8' |'9' | '10' | 'breakfast'
     
     ######### Gerund Phrase ############
     GER_P -> GER PP | GER
-    GER -> IV[FORM=prespart] | TV[FORM=prespart] NP
+    GER -> V[SUBCAT=intrans,FORM=prespart] | V[SUBCAT=trans, FORM=prespart] NP
     
     ######### Preposition Phrase #######
     PP  ->  P NP
     P   -> 'in' | 'on' | 'at' | 'before' | 'after' | 'from' | 'to'
     
     ######## Relative Clause ###########
-    REL_CL -> REL_P VP
+    REL_CL -> REL_P VP | 'that' S
     REL_P  -> 'that' | 'who'
     
     ######## Conjunction ###############
@@ -72,31 +74,39 @@ ugrammar = FeatureGrammar.fromstring("""\
     ################################### Verb #############################################
     ######################################################################################
     VP[FORM=?t, NUM=?n] -> ADV VP[FORM=?t, NUM=?n] | MODP[NUM=plur] VP[FORM=?t, NUM=plur]
-    VP[FORM=?t, NUM=?n] -> IV[FORM=?t, NUM=?n] | IV[FORM=?t, NUM=?n] PP
-    VP[FORM=?t, NUM=?n] -> TV[FORM=?t, NUM=?n] NP | TV[FORM=?t, NUM=?n] NP NP | TV[FORM=?t, NUM=?n] S 
-    VP[FORM=?t, NUM=?n] -> VTB[FORM=?t, NUM=?n] ADJ
+    VP[FORM=?t, NUM=?n] -> V[SUBCAT=intrans, FORM=?t] | V[SUBCAT=intrans,FORM=?t] PP
+    VP[FORM=?t, NUM=?n] -> V[SUBCAT=trans, FORM=?t] NP | V[SUBCAT=trans, FORM=?t] NP NP | V[SUBCAT=trans, FORM=?t] S 
+    VP[FORM=?t, NUM=?n] -> V[SUBCAT=vtb, FORM=?t, NUM=?n] ADJ
+    VP[FORM=?t, NUM=?n] -> V[SUBCAT=clause, TENSE=?t] REL_CL
     
     ######## Verb To Be ################
-    VTB[FORM=base, NUM=plur] -> 'are'
-    VTB[FORM=base, NUM=sing] -> 'is' | 'am'
-    VTB[FORM=vbz, NUM=plur]  -> 'were'
-    VTB[FORM=vbz, NUM=sing]  -> 'was'
-    VTB[FORM=pret]           -> 'been'
-    VTB[FORM=prespart]       -> 'being'
+    V[SUBCAT=vtb, FORM=base, NUM=plur] -> 'are'
+    V[SUBCAT=vtb, FORM=base, NUM=sing] -> 'is' | 'am'
+    V[SUBCAT=vtb, FORM=vbz, NUM=plur]  -> 'were'
+    V[SUBCAT=vtb, FORM=vbz, NUM=sing]  -> 'was'
+    V[SUBCAT=vtb, FORM=pret]           -> 'been'
+    V[SUBCAT=vtb, FORM=prespart]       -> 'being'
     
     ######## Intransitive Verb #########
-    IV[FORM=base, NUM=plur]     -> 'drink'      | 'serve'   | 'laugh'    | 'leave'  | 'live'
-    IV[FORM=vbz,  NUM=sing]     -> 'drinks'     | 'serves'  | 'laughs'   | 'leaving'| 'living'    
-    IV[FORM=pret]               -> 'drank'      | 'served'  | 'laughed'  | 'left'   | 'lived'
-    IV[FORM=pastpart]           -> 'drunk'      | 'served'  | 'laughed'  | 'left'   | 'lived'
-    IV[FORM=prespart]           -> 'drinking'   | 'serving' | 'laughing' | 'leaving'| 'living'
+    V[SUBCAT=intrans, FORM=base]       -> 'drink'      | 'serve'   | 'laugh'    | 'leave'  | 'live'
+    V[SUBCAT=intrans, FORM=vbz]        -> 'drinks'     | 'serves'  | 'laughs'   | 'leaves' | 'lives'    
+    V[SUBCAT=intrans, FORM=pret]       -> 'drank'      | 'served'  | 'laughed'  | 'left'   | 'lived'
+    V[SUBCAT=intrans, FORM=pastpart]   -> 'drunk'      | 'served'  | 'laughed'  | 'left'   | 'lived'
+    V[SUBCAT=intrans, FORM=prespart]   -> 'drinking'   | 'serving' | 'laughing' | 'leaving'| 'living'
     
     ######## Transitive Verb ###########
-    TV[FORM=base, NUM=plur]     -> 'drink'      | 'wear'    | 'serve'    | 'think'   | 'like'  | 'see'
-    TV[FORM=vbz,  NUM=sing]     -> 'drinks'     | 'wears'   | 'serves'   | 'thinks'  | 'likes' | 'sees'
-    TV[FORM=pret]               -> 'drank'      | 'wore'    | 'served'   | 'thought' | 'liked' | 'saw'
-    TV[FORM=pastpart]           -> 'drunk'      | 'worn'    | 'served'   | 'thought' | 'liked' | 'seen'
-    TV[FORM=prespart]           -> 'drinking'   | 'wearing' | 'serving'  | 'thinking'| 'liking'| 'seeing'
+    V[SUBCAT=trans, FORM=base]         -> 'drink'      | 'wear'    | 'serve'    | 'think'   | 'like'  | 'see'
+    V[SUBCAT=trans, FORM=vbz]          -> 'drinks'     | 'wears'   | 'serves'   | 'thinks'  | 'likes' | 'sees'
+    V[SUBCAT=trans, FORM=pret]         -> 'drank'      | 'wore'    | 'served'   | 'thought' | 'liked' | 'saw'
+    V[SUBCAT=trans, FORM=pastpart]     -> 'drunk'      | 'worn'    | 'served'   | 'thought' | 'liked' | 'seen'
+    V[SUBCAT=trans, FORM=prespart]     -> 'drinking'   | 'wearing' | 'serving'  | 'thinking'| 'liking'| 'seeing'
+    
+    ######## Clause ####################
+    V[SUBCAT=clause, TENSE=base]        -> 'say'    | 'claim'
+    V[SUBCAT=clause, TENSE=base]        -> 'says'   | 'claims'
+    V[SUBCAT=clause, TENSE=vbz]         -> 'said'   | 'claimed'
+    V[SUBCAT=clause, TENSE=pastpart]    -> 'said'   | 'claimed'
+    V[SUBCAT=clause, TENSE=prespart]    -> 'saying' | 'claiming'
 """)
 
 uparser = FeatureChartParser(ugrammar)
@@ -127,6 +137,7 @@ did the plane leave
 all the morning trains from Edinburgh to London leave before 10
 most flights that serve breakfast leave at 9
 the people who live in the house are friendly
+Lisa claims that Bart always leaves before 8
 """
 sents = text.splitlines()
 for sent in sents:
